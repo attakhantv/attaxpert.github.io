0f24c6016e2d38bca191c7bbfa3cbf1e7dfc48b6
Atta Xeprt <sangotr3@gmail.com>
2024-07-01 06:44:23 -0500

    New Version

---
+++ _next/static/chunks/app/page-3f119cb2b8076439.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{502:function(n,e,u){Promise.resolve().then(u.t.bind(u,1844,23))}},function(n){n.O(0,[844,749,422,744],function(){return n(n.s=502)}),_N_E=n.O()}]);
